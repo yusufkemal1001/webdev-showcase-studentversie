e4de4ed9f1e2fd38c9d8a4590ec88f1e670f1f01
Karen Brakband <xr0133363@windesheim.nl>
2024-08-30 14:01:09 +0200

    Initial commit

---
+++ US1-Profielpagina/Showcase-Profielpagina/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Showcase_Profielpagina.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Topteam()
+        {
+            return View();
+        }
+    }
+}
---
+++ US2-Contactpagina/ShowcaseAPI/Controllers/MailController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShowcaseAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShowcaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        // POST api/<MailController>
+        [HttpPost]
+        public ActionResult Post([Bind("FirstName, LastName, Email, Phone")] Contactform form)
+        {
+            //Op brightspace staan instructies over hoe je de mailfunctionaliteit werkend kunt maken:
+            //Project Web Development > De showcase > Week 2: contactpagina (UC2) > Hoe verstuur je een mail vanuit je webapplicatie met Mailtrap?
+            
+            return Ok();
+        }
+    }
+}
