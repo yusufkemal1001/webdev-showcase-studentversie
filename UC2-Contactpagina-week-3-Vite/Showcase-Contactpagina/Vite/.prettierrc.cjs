dff308bf255c4b49a3c2c7d0729f01724b0f54a3
Ernst Bolt <eb0095856@windesheim.nl>
2025-01-29 16:05:04 +0100

    Vite setup toegevoegd

---
+++ UC2-Contactpagina-week-3-Vite/Showcase-Contactpagina/Vite/.prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    /* for endOfLine, tabWidth, useTabs, and printWidth, see .editorconfig */
+    trailingComma: "es5",
+    semi: true,
+    singleQuote: false,
+    arrowParens: "avoid",
+    bracketSpacing: true,
+  };
